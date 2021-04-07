6e1b0c905bd2ecf189163a40b31a7b31ee07eceb
Jeck Castillo <breezelback@gmail.com>
2021-04-07 13:17:13 +0800

    Update fetchLogs.php
    Update product download order log

--- admin/function php/fetchLogs.php
+++ admin/function php/fetchLogs.php
@@ -3,24 +3,25 @@ require "conn.php";
 
 $borrowerId = $_GET['borrowerId'];
 
-$sql = ' SELECT `id`, `bookId`, `downloaderId`, `dateDownloaded`, `borrowerId`, `borrowerName` FROM `tbl_downloads` WHERE `borrowerId` = "'.$borrowerId.'" ';
+$sql = ' SELECT `id`, `bookId`, `downloaderId`, `dateDownloaded`, `borrowerId`, `borrowerName` FROM `tbl_downloads` WHERE `borrowerId` = "'.$borrowerId.'"  ORDER BY `dateDownloaded` DESC ';
 $exec = $conn->query($sql);
-while ($res = $exec->fetch_assoc()) {
+while ($res = $exec->fetch_assoc()) 
+{
 
-$phpdate = strtotime( $res['dateDownloaded'] );
-$mysqldate = date( 'M d, Y', $phpdate );
+	$phpdate = strtotime( $res['dateDownloaded'] );
+	$mysqldate = date( 'M d, Y', $phpdate );
 
 
-$selectBook = ' SELECT `filename`, `title` FROM `tbl_knowledge_products` WHERE `id` = "'.$res['bookId'].'" ';
-$execBook = $conn->query($selectBook);
-$resBook = $execBook->fetch_assoc();
+	$selectBook = ' SELECT `filename`, `title` FROM `tbl_knowledge_products` WHERE `id` = "'.$res['bookId'].'" ';
+	$execBook = $conn->query($selectBook);
+	$resBook = $execBook->fetch_assoc();
 
-?>
+	?>
 
-<tr>
-	<td><?php echo $resBook['title']; ?></td>
-	<td><?php echo $mysqldate; ?></td>
-</tr>
+	<tr>
+		<td><?php echo $resBook['title']; ?></td>
+		<td><?php echo $mysqldate; ?></td>
+	</tr>
 
 
 
