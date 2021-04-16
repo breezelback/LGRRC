edee72e5e390215c138fb9f8f717ac128cd29203
Jeck Castillo <breezelback@gmail.com>
2021-04-16 13:23:39 +0800

    Update product_history.php
    Remove bar chart code

--- admin/dashboard/views/product_history.php
+++ admin/dashboard/views/product_history.php
@@ -104,60 +104,6 @@ for ($i=1; $i < 13; $i++)
       }
     }
   });
-  // -- Bar Chart Example
-  var ctx = document.getElementById("myBarChart");
-  var myLineChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["January", "February", "March", "April", "May", "June"],
-      datasets: [{
-        label: "Revenue",
-        backgroundColor: "rgba(2,117,216,1)",
-        borderColor: "rgba(2,117,216,1)",
-        data: [4215, 5312, 6251, 7841, 9821, 14984],
-      }],
-    },
-    options: {
-      scales: {
-        xAxes: [{
-          time: {
-            unit: 'month'
-          },
-          gridLines: {
-            display: false
-          },
-          ticks: {
-            maxTicksLimit: 6
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            min: 0,
-            max: 15000,
-            maxTicksLimit: 5
-          },
-          gridLines: {
-            display: true
-          }
-        }],
-      },
-      legend: {
-        display: false
-      }
-    }
-  });
-  // -- Pie Chart Example
-  var ctx = document.getElementById("myPieChart");
-  var myPieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: ["Blue", "Red", "Yellow", "Green"],
-      datasets: [{
-        data: [12.21, 15.58, 11.25, 8.32],
-        backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-      }],
-    },
-  });
 
-
+  
 </script>
