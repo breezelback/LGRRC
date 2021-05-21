d97b00a65783c0aec2d2821a2e8ec3e8f244cc4e
Jeck Castillo <breezelback@gmail.com>
2021-05-21 09:27:28 +0800

    update conn
    admin conn

--- admin/function php/Connection.php
+++ admin/function php/Connection.php
@@ -13,15 +13,15 @@ class Connection
 
 	function connect()
 	{
-		// $db_name="calabarzondilggo_lgrrc";
-		// $mysql_username="calabarzondilggo_lgrrcuser";
-		// $mysql_password="`(q/*kTRV366'mqD@=eS-";
-		// $server_name="localhost";
-
-		$db_name="db_lgrrc";
-		$mysql_username="root";
-		$mysql_password='';
+		$db_name="calabarzondilggo_lgrrc";
+		$mysql_username="calabarzondilggo_lgrrcuser";
+		$mysql_password="`(q/*kTRV366'mqD@=eS-";
 		$server_name="localhost";
+
+		// $db_name="db_lgrrc";
+		// $mysql_username="root";
+		// $mysql_password='';
+		// $server_name="localhost";
 
 		$conn=mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
 		return $conn;
