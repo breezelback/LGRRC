2c8203f901cf6439470de0fffb5013ecfce4b3f3
Jeck Castillo <breezelback@gmail.com>
2021-06-22 13:58:18 +0800

    Admin Content
    Update structure

--- admin/directory/views/expert_table.php
+++ admin/directory/views/expert_table.php
@@ -11,8 +11,8 @@
     </tr>
   </thead>
   <tbody>
+   <?php foreach ($experts as $key => $expert): ?> 
     <tr>
-     <?php foreach ($experts as $key => $expert): ?> 
       <td><?php echo '<center><img id="td_image'.$expert["id"].'" data-data0="../images/expert/'.$expert["imageName"].'" src="../images/expert/'.$expert["imageName"].'" style="height:50px;width:50px;" class="img-rounded"></center>' ?></td>
       <td><?php echo '<div id="td_name'.$expert["id"].'" data-data1="'.$expert["name"].'">'.$expert["name"].'</div>'; ?></td>
       <td><?php echo '<div id="td_address'.$expert["id"].'" data-data3="'.$expert["address"].'">'.$expert["address"].'</div>';?></td>
