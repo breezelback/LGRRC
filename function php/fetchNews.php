d923720785876de27ac65fcaea183b0418f9d529
Jeck Castillo <58018826+breezelback@users.noreply.github.com>
2022-02-02 14:51:27 +0800

    Fetch News
    Resize news image

--- function php/fetchNews.php
+++ function php/fetchNews.php
@@ -20,7 +20,7 @@ $description = html_entity_decode($res['description']);
 $newDate = date("M d-Y | h:i A", strtotime($res['dateUploaded']));
  ?>
 
- <center><img class="card-img-top img-fluid" src="images/news/<?php echo $res['imageName']; ?>" alt="Card image cap" style="height: 500px; max-width: 80%;"></center>
+ <center><img class="card-img-top img-fluid" src="images/news/<?php echo $res['imageName']; ?>" alt="Card image cap" style="height: 500px; max-width: 70%;"></center>
  <center><h4 class="mt-2"><b><?php echo $res['author']; ?></b></h4></center>
  <center><h6><?php echo $res['title']; ?> </h6></center>
  <center><p><?php echo $newDate; ?></p></center><hr>
